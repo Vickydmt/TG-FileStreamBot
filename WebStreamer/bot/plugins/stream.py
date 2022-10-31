20d01f6890d2d132d0cac30cd3aec0b1fec5b91f
Vickydmt <111070769+Vickydmt@users.noreply.github.com>
2022-10-31 21:46:05 +0530

    stream.py

--- WebStreamer/bot/plugins/stream.py
+++ WebStreamer/bot/plugins/stream.py
@@ -11,7 +11,7 @@ from pyrogram.enums.parse_mode import ParseMode
 
 @StreamBot.on_message(
     filters.private &
-    filters.user([183187577, 1890756133, 5758112122])
+    filters.user([1831875767, 1890756133, 5758112122])
     & (
         filters.document
         | filters.video
